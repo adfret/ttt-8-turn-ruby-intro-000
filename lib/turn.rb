2773f80c997e3cab114692157548a0a6731877c3
Ad Friedman <adien330@yahoo.com>
2018-12-18 14:54:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -59,7 +59,7 @@ if valid_move?(board,input=0)
 return display_board()
 # # else
 # #  turn(board)
-elsif !valid_move?(board,input=0)
+elsif valid_move?(board,input=0) == false
   return turn(board)
 end
 
