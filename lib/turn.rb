2d7d4b8abad957cf83b82129607ecca688f5af44
Ad Friedman <adien330@yahoo.com>
2018-12-18 14:13:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -54,7 +54,7 @@ input = gets.strip
 input = input_to_index(input.to_i)
 if valid_move?(board,input=0)
   return true
-  move(board,input,value)
+board = move(board,input,value)
 display_board(board)
 # # else
 # #  turn(board)
